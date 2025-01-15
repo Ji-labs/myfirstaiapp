b8644e7480cc76790e3cd00c81bf03edf192e0ed
Ji-labs <52587692+Ji-labs@users.noreply.github.com>
2025-01-16 05:17:33 +0530

    Update Musk.py

--- Musk.py
+++ Musk.py
@@ -1,3 +1,15 @@
+%pip install langchain
+!pip install langchain_google_genai
+!pip install streamlit
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain import LLMChain
+from langchain.prompts import PromptTemplate
+import os
+os.environ['Google_API_Key']='AIzaSyAXgoiFtqArrLaHI4nTdnITNCWQrR2V1aY'
+tweet_template="Give me {number} tweets on {topic}"
+tweet_prompt=PromptTemplate(template=tweet_template,input_variables=['number','topic'])
+gemini_model = ChatGoogleGenerativeAI(model= "gemini-1.5-flash-latest")
+tweet_chain = tweet_prompt | gemini_model
 import streamlit as st
 st.header("Tweet Generator :)")
 st.subheader("Generate Tweets on any topic")
