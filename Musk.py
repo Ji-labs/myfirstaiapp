d716f78363f52faf306e375bf57c9a3ee0a5bea9
Ji-labs <52587692+Ji-labs@users.noreply.github.com>
2025-01-16 05:33:32 +0530

    Update Musk.py

--- Musk.py
+++ Musk.py
@@ -7,8 +7,7 @@ os.environ['Google_API_Key']='AIzaSyAXgoiFtqArrLaHI4nTdnITNCWQrR2V1aY'
 tweet_template="Give me {number} tweets on {topic}"
 tweet_prompt=PromptTemplate(template=tweet_template,input_variables=['number','topic'])
 gemini_model = ChatGoogleGenerativeAI(model= "gemini-1.5-flash-latest")
-tweet_generator = LLMChain(propmt = tweet_prompt, llm = gemini_model)
-tweet_chain = tweet_prompt | gemini_model
+tweet_generator = LLMChain(prompt = tweet_prompt, llm = gemini_model)
 st.header("Tweet Generator :)")
 st.subheader("Generate Tweets on any topic")
 topic = st.text_input("Topic")
